770b518d5a66185f6c05e5bb932c80bc32880a34
Manuel Alberto Velasquez Castaño <manuel.velasquez@ceiba.com.co>
2022-04-13 01:17:22 -0500

    refactorizacion de parametros

--- Clinica/angular-base/src/app/feature/solicitud/shared/model/solicitud-data.spec.ts
+++ Clinica/angular-base/src/app/feature/solicitud/shared/model/solicitud-data.spec.ts
@@ -4,12 +4,14 @@ describe('SolicitudData', () => {
   it('should create an instance', () => {
     expect(new SolicitudData(
       '1',
-      '1117522442',
-      'Juan Perez',
-      'Manuel Velasquez',
-      'General',
-      '2022-04-28',
-      '2022-04-09 00:00:00',
+      {
+        identificacionUsuario: '1117522442',
+        medico: 'Juan Perez',
+        usuario: 'Manuel Velasquez',
+        especialidad: 'General',
+        fechaCita: '2022-04-28',
+        fechaSolicitud: '2022-04-09 00:00:00',
+      },
       '07:00 AM',
       '07:30 AM',
       50000,
